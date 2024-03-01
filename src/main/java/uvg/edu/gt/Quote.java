2625c714b8a9e16a522b6c5b2ca6ef2475f317fd
Poposit <131786187+Dcextrim@users.noreply.github.com>
2024-03-01 17:07:29 -0600

    Clase Quote (V.1.0)

---
+++ src/main/java/uvg/edu/gt/Quote.java
@@ -0,0 +1,12 @@
+package uvg.edu.gt;
+
+public class Quote {
+
+    public Quote() {
+    }
+
+    public String returnQuote(String quote) {
+        System.out.println(quote);
+        return quote;
+    }
+}
