b941eb6642f332052ec9e3c415fb65f027b4832a
Poposit <131786187+Dcextrim@users.noreply.github.com>
2024-03-06 19:41:55 -0600

    Documentación Predicados

--- src/main/java/uvg/edu/gt/Predicados.java
+++ src/main/java/uvg/edu/gt/Predicados.java
@@ -1,10 +1,29 @@
 package uvg.edu.gt;
 
+/**
+ * Clase Predicados contiene métodos para procesar operaciones basadas en
+ * predicados.
+ * 
+ * @author Cristian Túnchez, Daniel Chet
+ * @version 1.0
+ * @since 24/03/2024
+ */
 public class Predicados {
 
+    /**
+     * Constructor por defecto.
+     */
     public Predicados() {
     }
 
+    /**
+     * Método que procesa la instrucción dada y devuelve el resultado de la
+     * operación
+     * según el predicado especificado.
+     *
+     * @param instrucc la instrucción que contiene la operación y sus operandos
+     * @return el resultado de la operación según el predicado
+     */
     public String evaluarPredicado(String instrucc) {
         String respuesta = "";
         boolean atom = false;
@@ -13,12 +32,16 @@ public class Predicados {
         boolean mayorQue = false;
         boolean menorQue = false;
 
+        // Separar la instrucción en partes y rearmarla
         String[] SaltoInstruccion = instrucc.split(" ");
         String expsplit = "";
         expsplit = String.join(" ", SaltoInstruccion);
 
+        // Obtener el primer carácter de la instrucción para determinar el tipo de
+        // operación
         String letra = expsplit.substring(0, 1);
 
+        // Establecer los predicados según el primer carácter
         if (letra.equalsIgnoreCase("a")) {
             atom = true;
         }
@@ -34,6 +57,8 @@ public class Predicados {
         if (letra.equals("<")) {
             menorQue = true;
         }
+
+        // Realizar la operación correspondiente según el predicado
 
         if (atom == true) {
             String[] SaltoEspacio = expsplit.split(" ");
@@ -58,6 +83,7 @@ public class Predicados {
         }
 
         if (list == true) {
+            // Imprimir los elementos de la lista
             System.out.print("( ");
             for (int i = 1; i <= (SaltoInstruccion.length - 1); i++) {
                 if (i < (SaltoInstruccion.length - 1)) {
