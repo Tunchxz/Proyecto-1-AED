268e1643ea91a915d694dd94d38b7476ffbfe942
Poposit <131786187+Dcextrim@users.noreply.github.com>
2024-03-19 16:45:03 -0600

    Clase Interprete (V.0.2)

--- src/main/java/uvg/edu/gt/Interprete.java
+++ src/main/java/uvg/edu/gt/Interprete.java
@@ -56,4 +56,95 @@ public class Interprete {
         }
         return "null";
     }
+
+    public String regresarParametro(String nombrefuncion) {
+        for (int i = 0; i < stackArraylist.size(); i++) {
+            if (nombrefuncion.equals(stackArraylist.get(i).getNombre())) {
+                return stackArraylist.get(i).getParametro();
+            }
+        }
+        return "null";
+    }
+
+    public StackVector<String> cambiarParametro(StackVector<String> stackVector, String parametro, String valor) {
+        StackVector<String> valores = new StackVector<String>();
+        for (int i = 0; i < stackVector.size(); i++) {
+            String llave = stackVector.get(i);
+            if (llave.equals(parametro)) {
+                valores.push(valor);
+            } else {
+                valores.push(llave);
+            }
+        }
+        return valores;
+    }
+
+    public boolean esNumero(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public void cambiarTrue(String lineacodigo) {
+        stackVector.clear();
+        String nuevoCodigo = "";
+        String nuevaLinea = lineacodigo.replace("(", "( ");
+        nuevaLinea = nuevaLinea.replace(")", " )");
+        String[] lineasplit = nuevaLinea.split(" ");
+        StackVector<String> nuevoVector = new StackVector<String>();
+        for (int i = 0; i < lineasplit.length; i++) {
+            nuevoVector.push(lineasplit[i]);
+        }
+        int contadorParentesisAbierto = 0;
+        int contadorParentesisCerrado = 0;
+        for (int i = 0; i < nuevoVector.size(); i++) {
+            String llave = nuevoVector.get(i);
+            if (llave.equals("(")) {
+                contadorParentesisAbierto++;
+            } else if (llave.equals(")")) {
+                contadorParentesisCerrado++;
+            }
+            if (contadorParentesisAbierto == 5 && contadorParentesisCerrado != 5 && (llave.equals("(") == false)
+                    && (llave.equals(")") == false)) {
+                nuevoCodigo += llave;
+                nuevoCodigo += " ";
+            }
+        }
+        verificarSintaxis(nuevoCodigo);
+    }
+
+    public void cambiarFalse(String lineacodigo) {
+        stackVector.clear();
+        String nuevoCodigo = "";
+        String nuevaLinea = lineacodigo.replace("(", "( ");
+        nuevaLinea = nuevaLinea.replace(")", " )");
+        String[] lineasplit = nuevaLinea.split(" ");
+        StackVector<String> nuevoVector = new StackVector<String>();
+        for (int i = 0; i < lineasplit.length; i++) {
+            nuevoVector.push(lineasplit[i]);
+        }
+        int contadorParentesisAbierto = 0;
+        int contadorParentesisCerrado = 0;
+        for (int i = 0; i < nuevoVector.size(); i++) {
+            String llave = nuevoVector.get(i);
+            if (llave.equals("(")) {
+                contadorParentesisAbierto++;
+            } else if (llave.equals(")")) {
+                contadorParentesisCerrado++;
+            }
+            if (contadorParentesisAbierto >= 6 && contadorParentesisCerrado != 6 && (llave.equals("(") == false)
+                    && (llave.equals(")") == false)) {
+                nuevoCodigo += llave;
+                nuevoCodigo += " ";
+            }
+        }
+        verificarSintaxis(nuevoCodigo);
+    }
+
+    public StackArrayList<Defun> getStackArraylist() {
+        return stackArraylist;
+    }
 }
