c399bf4f12caf821fa805c46cae2568dd8f19b2e
Poposit <131786187+Dcextrim@users.noreply.github.com>
2024-03-23 22:37:41 -0600

    Documentación Main

--- src/main/java/uvg/edu/gt/Main.java
+++ src/main/java/uvg/edu/gt/Main.java
@@ -1,3 +1,12 @@
+/**
+ * Clase principal que proporciona una interfaz para probar el intérprete de Common Lisp a partir de un archivo.
+ * 
+ * Este intérprete utiliza el Java Collections Framework para sus operaciones.
+ * 
+ * @author Cristian Túnchez, Daniel Chet
+ * @version 1.0
+ * @since 24/03/2024
+ */
 package uvg.edu.gt;
 
 import java.io.BufferedReader;
@@ -7,22 +16,28 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
+        // Iniciar la instancia del intérprete
         Interprete interprete = new Interprete();
 
+        // Intentar leer el archivo codigo.lisp
         try {
+            // Ruta del archivo
             File file = new File("src\\main\\java\\uvg\\edu\\gt\\Codigo.lisp");
             FileInputStream fileInputStream = new FileInputStream(file);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
 
+            // Mensaje de bienvenida
             System.out.println(
                     "--------------- Intérprete de Common Lisp (Por: Daniel Chet y Cristian Túnchez) ---------------");
 
+            // Ciclo para leer el código línea por línea
             String linea;
             while ((linea = bufferedReader.readLine()) != null) {
                 System.out.println("\n>>> " + linea);
                 interprete.interpretar(linea);
             }
 
+            // Cerrar el flujo de lectura
             bufferedReader.close();
         } catch (Exception e) {
             System.out.println("Error al leer el archivo: " + e.getMessage());
